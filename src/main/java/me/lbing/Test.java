c87579ad8c2d495eca2f9353cb8f44311a31d666
JeremyKing <jgroups@163.com>
2016-08-02 10:22:41 +0800

    Initial commit --hello world

---
+++ src/main/java/me/lbing/Test.java
@@ -0,0 +1,25 @@
+package me.lbing;
+
+import org.apache.log4j.Logger;
+
+/**
+ * Created by King on 2016/8/2.
+ */
+public class Test {
+    private static Logger logger = Logger.getLogger(Test.class);
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        // System.out.println("This is println message.");
+
+        // 记录debug级别的信息
+        logger.debug("This is debug message.");
+        // 记录info级别的信息
+        logger.info("This is info message.");
+        // 记录error级别的信息
+        logger.error("This is error message.");
+    }
+
+}
